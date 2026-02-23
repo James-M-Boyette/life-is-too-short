f063e682639ec4b34fb011cb630b5c060096b474
James-M-Boyette <James.M.84.Boyette@gmail.com>
2026-02-22 23:29:21 -0500

    feature: create app-shell

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,35 @@
 import { Routes } from '@angular/router';
 import { authedOnlyGuard } from '@/shared/supabase/authed-only.guard';
-import { LoginPage } from '@/features/auth/login.page';
+import { LoginPage } from '@/features/auth/pages/login.page';
 import { TasksPage } from '@/features/tasks/pages/tasks.page';
-import { AuthCallbackPage } from '@/features/auth/auth-callback.page';
+import { AuthCallbackPage } from '@/features/auth/pages/auth-callback.page';
 
+// export const routes: Routes = [
+//     { path: '', pathMatch: 'full', redirectTo: 'tasks' },
+//     { path: 'login', component: LoginPage },
+//     { path: 'auth/callback', component: AuthCallbackPage },
+//     { path: 'tasks', component: TasksPage, canMatch: [authedOnlyGuard] },
+//     { path: '**', redirectTo: 'tasks' },
+// ];
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'tasks' },
-    { path: 'login', component: LoginPage },
-    { path: 'auth/callback', component: AuthCallbackPage },
-    { path: 'tasks', component: TasksPage, canMatch: [authedOnlyGuard] },
-    { path: '**', redirectTo: 'tasks' },
+    {
+        path: 'login',
+        loadComponent: () => import('@/features/auth/pages/login.page').then((m) => m.LoginPage),
+    },
+
+    {
+        path: '',
+        loadComponent: () =>
+            import('@/layout/app-shell/app-shell.component').then((m) => m.AppShellComponent),
+        children: [
+            {
+                path: 'tasks',
+                loadComponent: () =>
+                    import('@/features/tasks/pages/tasks.page').then((m) => m.TasksPage),
+            },
+            { path: '', pathMatch: 'full', redirectTo: 'tasks' },
+        ],
+    },
+
+    { path: '**', redirectTo: '' },
 ];
